f90244924d1830db66698645c7a8e64378d62c16
autumn_dima <ozerland@gmail.com>
2024-03-14 04:05:03 +0200

    improve project configs

--- eslint.config.js
+++ eslint.config.js
@@ -5,38 +5,50 @@ import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked, {
-    plugins: {
-        ['import']: importPlugin,
-        ['simple-import-sort']: simpleImportSortPlugin,
-    },
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.es2020,
-            ...globals.node,
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    {
+        plugins: {
+            ['import']: importPlugin,
+            ['simple-import-sort']: simpleImportSortPlugin,
         },
-        parserOptions: {
-            allowAutomaticSingleRunInference: true,
-            cacheLifetime: {
-                // we pretty well never create/change tsconfig structure - so no need to ever evict the cache
-                // in the rare case that we do - just need to manually restart their IDE.
-                glob: 'Infinity',
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.es2020,
+                ...globals.node,
             },
-            project: ['tsconfig.json', 'apps/*/tsconfig.json', 'packages/*/tsconfig.json'],
-            tsconfigRootDir: import.meta.dirname,
-            warnOnUnsupportedTypeScriptVersion: false,
+            parserOptions: {
+                allowAutomaticSingleRunInference: true,
+                cacheLifetime: {
+                    // we pretty well never create/change tsconfig structure - so no need to ever evict the cache
+                    // in the rare case that we do - just need to manually restart their IDE.
+                    glob: 'Infinity',
+                },
+                project: [
+                    'tsconfig.json',
+                    'apps/*/tsconfig.json',
+                    'packages/*/tsconfig.json',
+                    'apps/client/tsconfig.node.json',
+                ],
+                tsconfigRootDir: import.meta.dirname,
+                warnOnUnsupportedTypeScriptVersion: false,
+            },
+        },
+        rules: {
+            '@typescript-eslint/consistent-type-imports': 'error',
+
+            'import/first': 'error',
+            'import/newline-after-import': 'error',
+            'import/no-absolute-path': 'error',
+            'import/no-mutable-exports': 'error',
+            'import/no-self-import': 'error',
+
+            'simple-import-sort/imports': 'error',
         },
     },
-    rules: {
-        '@typescript-eslint/consistent-type-imports': 'error',
-
-        'import/first': 'error',
-        'import/newline-after-import': 'error',
-        'import/no-absolute-path': 'error',
-        'import/no-mutable-exports': 'error',
-        'import/no-self-import': 'error',
-
-        'simple-import-sort/imports': 'error',
+    {
+        ignores: ['**/dist/**', '**/node_modules/**'],
     },
-});
+);
